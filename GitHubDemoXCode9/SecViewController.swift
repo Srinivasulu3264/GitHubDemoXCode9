26fd237435d2bdd1231c1d8916fa1fe2a93c5ddb
Srinivasulu3264 <srinivasulu.budharapu@vmokshagroup.com>
2017-12-26 12:59:37 +0530

    added secVc

---
+++ GitHubDemoXCode9/SecViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecViewController.swift
+//  GitHubDemoXCode9
+//
+//  Created by Vmoksha on 26/12/17.
+//  Copyright © 2017 Srinivas. All rights reserved.
+//
+
+import UIKit
+
+class SecViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
